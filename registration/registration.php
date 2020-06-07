9430beb253cd9c98c690e0c9512bc6cb9b8fcc70
Malte Becker <malte91@web.de>
2020-06-07 15:33:00 +0200

    add class, add event to clean table

--- registration/registration.php
+++ registration/registration.php
@@ -7,50 +7,88 @@ Author: Malte Becker
 */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-// Function is called on activating the plugin.
-function createTable()
+$start_registator = new Registrator();
+
+class Registrator
 {
-    global $wpdb;
-    $table_name = $wpdb->prefix . "registration";
-
-    $charset_collate = $wpdb->get_charset_collate();
-
-    if ($wpdb->get_var("SHOW TABLES LIKE '". $table_name ."'"  ) != $table_name ) 
+    public function __construct()
     {
-        $sql = "CREATE TABLE $table_name (
-            id mediumint(9) NOT NULL AUTO_INCREMENT,
-            name tinytext NOT NULL,
-            age tinytext NOT NULL,
-            PRIMARY KEY  (id)
-        ) $charset_collate;";
+        register_activation_hook( __FILE__, array( $this, 'registratorActivation') );
+        register_deactivation_hook( __FILE__, array( $this, 'registratorDeactivation') );
 
-        if(!function_exists('dbDelta'))
-        {
-            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        }
-        dbDelta( $sql );
+        add_action( 'cleanTableEvent', array( $this, 'clean_table'));
     }
-}
-register_activation_hook( __FILE__, 'createTable' );
 
-// Returns number of subscribers of current day with given age
-function getSubscribers($age)
-{
+    // Public
+    public function registratorActivation()
+    {
+        $this->createTable();
+        if (! wp_next_scheduled ( 'cleanTableEvent' )) {
+			wp_schedule_event( strtotime('00:00:00'), 'daily', 'cleanTableEvent' );
+		}
+    }
 
-}
+    public function registratorDeactivation()
+    {
+        $this->deleteTable();
+        if ( wp_next_scheduled( 'cleanTableEvent' ) ) {
+			wp_clear_scheduled_hook( 'cleanTableEvent' );
+		}
+    }
 
-// Writes data from form to database
-function writeToDB($name, $age)
-{
+    // Private
+    function createTable()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "registration";
 
-}
+        $charset_collate = $wpdb->get_charset_collate();
 
-// Removes all entries from database
-function cleanDB()
-{
-    // global $wpdb;
-    // $table_name = $wpdb->prefix . "registration";
-    // $delete = $wpdb->query("TRUNCATE TABLE $table_name");
+        if ($wpdb->get_var("SHOW TABLES LIKE '". $table_name ."'"  ) != $table_name ) 
+        {
+            $sql = "CREATE TABLE $table_name (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                name tinytext NOT NULL,
+                age tinytext NOT NULL,
+                PRIMARY KEY  (id)
+            ) $charset_collate;";
+
+            if(!function_exists('dbDelta'))
+            {
+                require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            }
+            dbDelta( $sql );
+        }
+    }
+
+
+    function deleteTable()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "registration";
+        $wpdb->query( "DROP TABLE IF EXISTS $table_name" );
+    }
+
+
+    // Returns number of subscribers of current day with given age
+    function getSubscribers($age)
+    {
+
+    }
+
+    // Writes data from form to database
+    function writeToDB($name, $age)
+    {
+
+    }
+
+    // Removes all entries from database
+    function cleanTable()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "registration";
+        $delete = $wpdb->query("TRUNCATE TABLE $table_name");
+    }
 }
 
 ?>
--- registration/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
-    global $wpdb;
-    $table_name = $wpdb->prefix . "registration";
-    $wpdb->query( "DROP TABLE IF EXISTS $table_name" );
-    delete_option("registration");
-?>
